7274c9f2bab2337c750e4f1e6dc7addd206d3183
cypherdavy <166112437+cypherdavy@users.noreply.github.com>
2024-10-25 23:20:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showNextClue() {
+    document.getElementById("clueContainer").classList.remove("hidden");
+    document.getElementById("nextClue").classList.add("hidden");
+}
+
+function showFinalMessage() {
+    document.getElementById("finalMessage").classList.remove("hidden");
+}
